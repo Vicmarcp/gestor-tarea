7f14d792667e29f52736d0eeb6d7fbdac1035a19
Vicmarcp <victormarcosp@gmail.com>
2025-05-12 16:25:21 +0200

    Añadir eliminar_tarea

--- tareas.py
+++ tareas.py
@@ -5,3 +5,11 @@ def agregar_tarea(lista, tarea):
 def listar_tareas(lista):
     for i, t in enumerate(lista, 1):
         print(f"{i}. {t}")
+
+def eliminar_tarea(lista, indice):
+    if 1 <= indice <= len(lista):
+        lista.pop(indice-1)
+        return lista
+    else:
+        print("Índice inválido.")
+        return lista
